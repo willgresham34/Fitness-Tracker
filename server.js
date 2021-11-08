0bde93013783244781cc9f56eb3a9ccfe16119ce
Will Gresham <willgresham34@gmail.com>
2021-11-08 16:53:49 -0500

    server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(__dirname, "/public"));
+
+mongoose.connect(process.env.MONGODB_URI || "", {});
+
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/displayRoutes"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
