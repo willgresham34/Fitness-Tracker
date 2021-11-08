c6f5c1584ef6c94925b26c05c0ae29d276d809ec
Will Gresham <willgresham34@gmail.com>
2021-11-08 12:44:21 -0500

    get workout route done

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,28 @@
 const db = require("../models");
 const router = require("express").Router();
+
+//get workouts
+router.get("/api/workouts", async (req, res) => {
+  const workouts = await db.workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
+      },
+    },
+  ]);
+  try {
+    res.json(workouts);
+  } catch {
+    console.log(err);
+  }
+});
+
+//create workouts
+router.post("/api/workouts", async (req, res) => {});
+
+//put workout into db
+router.put("/api/workouts/:id", async (req, res) => {});
+
+module.exports = router;
