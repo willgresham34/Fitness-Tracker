ade8406323e0e9b69ed03859f05cdbc347c94a60
Will Gresham <willgresham34@gmail.com>
2021-11-09 14:33:00 -0500

    display routes

--- routes/displayRoutes.js
+++ routes/displayRoutes.js
@@ -1,4 +1,16 @@
 const db = require("../models");
 const router = require("express").Router();
 
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/stats", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+router.get("/exercise", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
 module.exports = router;
